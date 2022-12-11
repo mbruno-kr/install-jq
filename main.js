cf8a9ae0635bfb728db02d6658ba51588238a5fd
MBRound18 <12646562+mbround18@users.noreply.github.com>
2022-12-11 14:45:49 -0800

    yeet

---
+++ main.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const {spawnSync} = require("child_process");
+
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  let version = core.getInput('version');  
+  let fetchReleaseUrl = `https://api.github.com/repos/${}/${repo}/releases/latest`
+  if (version.toLowerCase === "latest") {
+    
+  }
+
+  spawnSync(`echo "test_var=true" >> $GITHUB_ENV`)
+
+  
+  
+
+
+  
+  
+} catch (error) {
+  core.setFailed(error.message);
+}
