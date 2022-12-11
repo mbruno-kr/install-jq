9aebd2ec8ee46990214369362936584de0060402
MBRound18 <12646562+mbround18@users.noreply.github.com>
2022-12-11 15:33:01 -0800

    build

--- src/main.js
+++ src/main.js
@@ -1,21 +1,22 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const {spawnSync} = require("child_process");
-const fs = require('fs');
-
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { spawnSync } = require("child_process");
+const fs = require("fs");
+const { glob } = require("glob");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  let version = core.getInput('version');  
-//   let fetchReleaseUrl = `https://api.github.com/repos/${}/${repo}/releases/latest`
+  let version = core.getInput("version");
+  //   let fetchReleaseUrl = `https://api.github.com/repos/${}/${repo}/releases/latest`
+  glob(
+    "/home/runner/work/_temp/_runner_file_commands/add_path_*",
+    (_e, (files) => console.log(files))
+  );
   if (version.toLowerCase === "latest") {
-    
   }
 
-  console.log(process.env["GITHUB_PATH"])
+  console.log(process.env["GITHUB_PATH"]);
   fs.appendFileSync(process.env["GITHUB_ENV"], "test_var=true");
-    
-  
 } catch (error) {
   core.setFailed(error.message);
 }
