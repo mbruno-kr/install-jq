4c8de952acc22cec0ae903041210c38d55b67c18
Michael B <michael.bruno2@kroger.com>
2022-12-12 14:54:41 -0800

    With proxy if proxy

---
+++ src/http.js
@@ -0,0 +1,22 @@
+let proxy;
+if (process.env["HTTPS_PROXY"]) {
+  proxy = new URL(process.env["HTTPS_PROXY"]);
+} else if (process.env["HTTP_PROXY"]) {
+  proxy = new URL(process.env["HTTP_PROXY"]);
+}
+
+const baseURL = "https://api.github.com";
+let client = axios.create({
+  baseURL,
+});
+if (proxy) {
+  client = axios.create({
+    baseURL,
+    proxy: {
+      host: proxy.host,
+      port: proxy.port,
+    },
+  });
+}
+
+export default client;
